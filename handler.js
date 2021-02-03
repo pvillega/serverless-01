e8aeee9788537493d793c60033a12d1f60145c62
Pere Villega <pvillega@aracon.com>
2021-02-03 11:33:21 +0000

    Config fixes for deployment

--- handler.js
+++ handler.js
@@ -1,13 +1,6 @@
 'use strict';
 
 module.exports.hello = async (event) => {
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: `Hello, the current time is ${new Date().toTimeString()}.`,
-  //   }),
-  // };
-
   return {
     statusCode: 200,
     body: JSON.stringify(
